00d99c8cea06019c4d21bddb66226e27f440cf1a
BenPickles85 <btpickles85@gmail.com>
2021-11-14 11:12:21 +1100

    completed section 1

---
+++ UFO-level-2/static/js/app.js
@@ -0,0 +1,81 @@
+// from data.js
+var tableData = data;
+
+// YOUR CODE HERE!
+
+var tbody = d3.select("tbody");
+// var dateInput = d3.select("#datetime");
+var button = d3.select("#filter-btn");
+var form = d3.select("form")
+
+
+// Render table on page load
+// tableData.forEach((sighting) => {
+            
+//     // Create table
+//     var row = tbody.append("tr");
+
+//     Object.entries(sighting).forEach(([key, value]) => {
+//             var cell = row.append("td");
+//             cell.text(value);
+//         })
+// });
+
+// Create event handlers for clicking the button or pressing the enter key
+button.on("click", renderTable);
+form.on("submit", renderTable);
+
+// Complete the event handler function for the form
+function renderTable() {
+
+    // Prevent the page from refreshing
+    
+
+    tableData.forEach((sighting) => {
+            
+        // Create table
+        var row = tbody.append("tr");
+    
+        Object.entries(sighting).forEach(([key, value]) => {
+                var cell = row.append("td");
+                cell.text(value);
+            })
+    });
+
+    d3.event.preventDefault();
+    
+    // Obtain input date and assign as variable
+    var inputDate = d3.select("#datetime");
+    
+    // Get the value property of the input element
+    var inputValue = inputDate.property("value");
+
+    console.log(inputDate);
+
+    var filteredData = tableData.filter(sighting => sighting.datetime === inputValue);
+    
+        // Render full table if date value cleared
+        if (filteredData.length == "0") {
+            filteredData = tableData
+        };
+
+    // Check data is populating
+    console.log(filteredData);
+
+    // Clear anything in the table
+    tbody.html("");
+    
+        filteredData.forEach((sighting) => {
+            
+            // Create table
+            var row = tbody.append("tr");
+
+            Object.entries(sighting).forEach(([key, value]) => {
+                    var cell = row.append("td");
+                    cell.text(value);
+                })
+        });
+};
+
+renderTable();
+
