cea4b11f9961c2666f18a4aedeaed91ef775753f
BenPickles85 <btpickles85@gmail.com>
2021-11-15 20:16:03 +1100

    completed section 2

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -8,28 +8,12 @@ var tbody = d3.select("tbody");
 var button = d3.select("#filter-btn");
 var form = d3.select("form")
 
-
-// Render table on page load
-// tableData.forEach((sighting) => {
-            
-//     // Create table
-//     var row = tbody.append("tr");
-
-//     Object.entries(sighting).forEach(([key, value]) => {
-//             var cell = row.append("td");
-//             cell.text(value);
-//         })
-// });
-
 // Create event handlers for clicking the button or pressing the enter key
 button.on("click", renderTable);
 form.on("submit", renderTable);
 
 // Complete the event handler function for the form
 function renderTable() {
-
-    // Prevent the page from refreshing
-    
 
     tableData.forEach((sighting) => {
             
@@ -42,6 +26,7 @@ function renderTable() {
             })
     });
 
+    // Prevent the page from refreshing
     d3.event.preventDefault();
     
     // Obtain input date and assign as variable
@@ -54,10 +39,10 @@ function renderTable() {
 
     var filteredData = tableData.filter(sighting => sighting.datetime === inputValue);
     
-        // Render full table if date value cleared
-        if (filteredData.length == "0") {
-            filteredData = tableData
-        };
+    // Render full table if date value cleared
+    if (filteredData.length == "0") {
+        filteredData = tableData
+    };
 
     // Check data is populating
     console.log(filteredData);
--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -8,19 +8,6 @@ var tbody = d3.select("tbody");
 var button = d3.select("#filter-btn");
 var form = d3.select("form")
 
-
-// Render table on page load
-// tableData.forEach((sighting) => {
-            
-//     // Create table
-//     var row = tbody.append("tr");
-
-//     Object.entries(sighting).forEach(([key, value]) => {
-//             var cell = row.append("td");
-//             cell.text(value);
-//         })
-// });
-
 // Create event handlers for clicking the button or pressing the enter key
 button.on("click", renderTable);
 form.on("submit", renderTable);
@@ -46,18 +33,55 @@ function renderTable() {
     
     // Obtain input date and assign as variable
     var inputDate = d3.select("#datetime");
+    var inputCity = d3.select("#city")
+    var inputState = d3.select("#state")
+    var inputCountry = d3.select("#country")
+    var inputShape = d3.select("#shape")
     
     // Get the value property of the input element
-    var inputValue = inputDate.property("value");
+    var inputDateValue = inputDate.property("value");
+    var inputCityValue = inputCity.property("value");
+    var inputStateValue = inputState.property("value");
+    var inputCountryValue = inputCountry.property("value");
+    var inputShapeValue = inputShape.property("value");
 
     console.log(inputDate);
+    console.log(inputCity);
+    console.log(inputState);
+    console.log(inputCountryValue);
+    console.log(inputShapeValue);
 
-    var filteredData = tableData.filter(sighting => sighting.datetime === inputValue);
-    
-        // Render full table if date value cleared
-        if (filteredData.length == "0") {
-            filteredData = tableData
-        };
+    var filteredData = tableData.filter((sighting) => {
+        
+        var matchesDate = false;
+        var matchesCity = false;
+        var matchesState = false;
+        var matchesCountry = false;
+        var matchesShape = false;
+
+        if (inputDateValue.length == '0' || sighting.datetime === inputDateValue) {
+            matchesDate = true;
+        }
+
+        if (inputCityValue.length == '0' || sighting.city.toLowerCase().includes(inputCityValue)) {
+            matchesCity = true;
+        }
+
+        if (inputStateValue.length == '0' || sighting.state.toLowerCase().includes(inputStateValue)) {
+            matchesState = true;
+        }
+
+        if (inputCountryValue.length == '0' || sighting.country.toLowerCase().includes(inputCountryValue)) {
+            matchesCountry = true;
+        }
+
+        if (inputShapeValue.length == '0' || sighting.shape.toLowerCase().includes(inputShapeValue)) {
+            matchesShape = true;
+        }
+
+        return matchesDate && matchesCity && matchesState && matchesCountry && matchesShape;
+
+    });
 
     // Check data is populating
     console.log(filteredData);
@@ -78,4 +102,5 @@ function renderTable() {
 };
 
 renderTable();
+
 
